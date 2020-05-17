a692379996c209255eb752cd708cca832876e1c7
Luke Bradaric <lukebradaric@gmail.com>
2020-05-17 15:18:49 -0700

    Users can add mp3 commands from chat.
    add [command] (mp3 attachment)

--- index.js
+++ index.js
@@ -1,142 +1,244 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const channelName = 'frostbolt';
+const useSpecificChannel = true;
 const initMessage = 'Frostbolt initialized.';
 const sendInitMessage = false;
-let connectedChannels = [];
 let regCommands = [];
+let connectedChannels = [];
 
-const tokenData = require('./token.json')
+let request = require('request');
+let fs = require('fs');
+
+const tokenData = require('./token.json');
 const token = tokenData.token;
 client.login(token);
 
-client.on('ready', () => {
-    console.log("Connected as " + client.user.tag);
+client.on('ready', () =>
+{
+    console.log('Connected as ' + client.user.tag);
 
-    client.user.setActivity("hentai", { type: "WATCHING" })
+    client.user.setActivity('hentai', { type: 'WATCHING' });
 
     //For each server, log that it has connected
-    client.guilds.cache.forEach((guild) => {
-
-        console.log("Connected to " + guild.name);
+    client.guilds.cache.forEach((guild) =>
+    {
+        console.log('Connected to ' + guild.name);
 
         //For each server with the designated channel name, add it to the connectedChannels array
-        guild.channels.cache.forEach((channel) => {
-            if (channel.name == channelName) {
+        for (let channel of guild.channels.cache)
+        {
+            if (channel.name == channelName)
+            {
                 let botChannel = client.channels.cache.get(channel.id);
                 connectedChannels.push(botChannel);
-                console.log("Connected to " + channelName + " in " + guild.name);
+                console.log('Connected to ' + channelName + ' in ' + guild.name);
             }
-        })
-
-    })
+        }
+    });
 
     //Load all commands from json file
     loadCommands();
 
     //If the bot is using Initlization messages, send it
-    if (sendInitMessage) {
-        connectedChannels.forEach(channel => channel.send(initMessage));
+    if (sendInitMessage)
+    {
+        for (let channel of connectedChannels)
+        {
+            channel.send(initMessage);
+        }
     }
-})
+});
 
-client.on('message', message => {
-
+client.on('message', (message) =>
+{
     //If message is bot or not designated channel, return
-    if (message.author == client.user || message.channel.name != channelName) {
+    if (message.author == client.user || message.channel.name != channelName && useSpecificChannel)
+    {
         return;
     }
 
+    //If is add message, and has a command after add prefix
+    if (message.content.substring(0, 3) == 'add' && message.content.length >= 5)
+    {
+        //If message has mp3 attachment, download it
+        if (message.attachments.first())
+        {
+            newCmd = message.content.substring(4, message.content.length).trim();
+
+            //Check if command already exists
+            for (let cmd of regCommands)
+            {
+                if (cmd.command == newCmd)
+                {
+                    console.log('Command ' + newCmd + ' already exists' + ' (' + message.member.user.tag + ')');
+                    replyToMessage(message, "Command already exists");
+                    return;
+                }
+            }
+
+            fn = message.attachments.first().name; //file name
+            fType = fn.substring(fn.length - 3, fn.length); //file type
+            if (fType == 'mp3')
+            {
+                console.log('Command: ' + newCmd + ', File: ' + newCmd + '.mp3');
+                console.log('Downloading mp3, requested by, ' + message.member.user.tag);
+                message.reply('Command added as: ' + "'" + newCmd + "'");
+                downloadMp3(message.attachments.first().url, newCmd);
+                addCommand(newCmd, newCmd + '.mp3');
+                return;
+            }
+        }
+    }
+
     //Check each command for a match
-    regCommands.forEach(cmd => {
-        if (message.content.includes(cmd.command)) {
+    for (let cmd of regCommands)
+    {
+        if (message.content.includes(cmd.command))
+        {
             const vc = message.member.voice.channel;
 
-            if (vc == null) {
+            if (vc == null)
+            {
                 return;
             }
 
             //If multiple files, pick a random one
             let fileToPlay;
-            if (isArray(cmd.file)) {
+            if (isArray(cmd.file))
+            {
                 let randomNum = Math.floor(Math.random() * cmd.file.length);
                 fileToPlay = cmd.file[randomNum];
-            } else {
+            } else
+            {
                 fileToPlay = cmd.file;
             }
 
             //Join channel then play file
-            vc.join().then(connection => {
+            vc.join().then((connection) =>
+            {
                 const dispatcher = connection.play(stringToSound(fileToPlay));
-                console.log("Sending command: " + cmd.command + ", requested by " + message.member.user.tag);
-                dispatcher.on('finish', finish => {
+                console.log(
+                    'Sending command: ' +
+                    cmd.command +
+                    ', requested by ' +
+                    message.member.user.tag
+                );
+                dispatcher.on('finish', (finish) =>
+                {
                     vc.leave();
-                })
+                });
             });
         }
-    })
-
-    if (message.content == "commands") {
-        cmds = getCommandList();
-        let channelId = message.channel.id;
-        client.channels.cache.get(channelId).send(cmds);
     }
-})
+
+    if (message.content == 'commands')
+    {
+        cmds = getCommandList();
+        respondToMessage(message, cmds);
+    }
+
+});
+
+function respondToMessage(message, reply)
+{
+    client.channels.cache.get(message.channel.id).send(reply);
+}
+
+//Downloads and mp3 from a message
+function downloadMp3(url, name)
+{
+    console.log("Downloading " + url);
+    request.get(url).on('error', console.error).pipe(fs.createWriteStream('sounds/' + newCmd + '.mp3'));
+}
 
 //Load all commands from json file
-function loadCommands() {
+function loadCommands()
+{
     regCommands = [];
 
     //Load json file
     const data = require('./commands.json');
 
     //Add all json objects to commands array
-    for (i = 0; i < data.length; i++) {
+    for (i = 0; i < data.length; i++)
+    {
         regCommands.push(data[i]);
     }
 
     //Notify for each command registered
-    regCommands.forEach(cmd => {
-        console.log("Registered command: " + cmd.command);
+    for (let cmd of regCommands)
+    {
+        console.log('Registered command: ' + cmd.command);
+    }
+}
+
+//Adds new command to regCommands array, and re-writes json commands
+function addCommand(command, file)
+{
+    let newData = { "command": command, "file": file }
+
+    console.log("Added " + command + " at " + file);
+    regCommands.push(newData);
+
+    let jsonObj = JSON.stringify(regCommands);
+    fs.writeFile('./commands.json', jsonObj, 'utf8', function (err)
+    {
+        if (err)
+        {
+            console.log("Error occured writing to json file");
+            return;
+        }
     })
+
 }
 
 //Returns a list of all available commands
-function getCommandList() {
-    let commandList = "commands: ";
-    regCommands.forEach(cmd => {
-        commandList += cmd.command + ", ";
-    })
+function getCommandList()
+{
+    let commandList = 'commands: ';
+    for (let cmd of regCommands)
+    {
+        commandList += cmd.command + ', ';
+    }
     return commandList;
 }
 
 //Return if the object is an array
-function isArray(obj) {
+function isArray(obj)
+{
     return Object.prototype.toString.call(obj) === '[object Array]';
 }
 
 //Return a string with path to the sound folder
-function stringToSound(str) {
+function stringToSound(str)
+{
     return 'sounds/' + str;
 }
 
 //Return a string with the path to the image folder
-function stringToImage(str) {
+function stringToImage(str)
+{
     return 'images/' + str;
 }
 
 //Return a string with path and file name supplied (kinda pointless?)
-function stringToFile(strPath, str) {
+function stringToFile(strPath, str)
+{
     return strPath + '/' + str;
 }
 
 //Return a string with every other letter uppercase
-function stringToAlt(str) {
+function stringToAlt(str)
+{
     let newStr = [];
-    for (i = 0; i < str.length; i++) {
-        if (i % 2 == 0) {
+    for (i = 0; i < str.length; i++)
+    {
+        if (i % 2 == 0)
+        {
             newStr.push(str.charAt(i).toUpperCase());
-        } else {
+        } else
+        {
             newStr.push(str.charAt(i).toLowerCase());
         }
     }
